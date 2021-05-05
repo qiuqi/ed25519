e5dce7bf223b2e1d66ed850c21c3cc7eb1ece14c
qiuqi <qiu.qiq@gmail.com>
2021-05-05 22:01:52 +0800

    fix bug: easy crash with wrong bad arg

--- src/ed25519_sup.erl
+++ src/ed25519_sup.erl
@@ -13,5 +13,5 @@ init([])->
     Server = {ed25519_server,
               {ed25519_server, start_link, []},
               permanent, 5000, worker, [ed25519_server]},
-    {ok, { {one_for_one, 5, 10}, [Server]}}.
+    {ok, { {one_for_one, 100, 1}, [Server]}}.
 
